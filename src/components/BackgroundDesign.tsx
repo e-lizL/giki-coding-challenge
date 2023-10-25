d6565084bbc9be71bb89fb85a668c1c93f8936a2
elizabeth lands <elizabeth_lands@hotmail.com>
2023-10-25 21:39:18 +0100

    feat: lay out background images, butterfly etc.

--- src/components/BackgroundDesign.tsx
+++ src/components/BackgroundDesign.tsx
@@ -17,10 +17,50 @@ const StyledImage = styled.img`
 
 const StyledPurpleCloud = styled(StyledImage)`
   width: 400px;
+  z-index: 10;
 `;
 
 const StyledWhiteCloud = styled(StyledImage)`
   width: 350px;
+  z-index: 30;
+`;
+
+const StyledButterfly = styled(StyledImage)`
+  width: 80px;
+  top: 23%;
+  left: 24%;
+  transform: translate(-30%, -30%);
+  z-index: 40;
+`;
+
+const StyledLeaf1a = styled(StyledImage)`
+  width: 70px;
+  top: 20%;
+  left: 71%;
+  z-index: 20;
+`;
+
+const StyledLeaf2a = styled(StyledImage)`
+  width: 100px;
+  top: 30%;
+  left: 75%;
+  z-index: 20;
+`;
+
+const StyledLeaf1b = styled(StyledImage)`
+  width: 70px;
+  top: 75%;
+  left: 21%;
+  transform: scale(-1, -1);
+  z-index: 20;
+`;
+
+const StyledLeaf2b = styled(StyledImage)`
+  width: 100px;
+  top: 70%;
+  left: 16%;
+  transform: scale(-1, -1);
+  z-index: 20;
 `;
 
 export default function BackgroundDesign() {
@@ -31,6 +71,16 @@ export default function BackgroundDesign() {
       <StyledPurpleCloud src="/purple_cloud.png" alt="purple cloud" />
 
       <StyledWhiteCloud src="/white_cloud.png" alt="white cloud" />
+
+      <StyledButterfly src="/butterfly.svg" alt="butterfly" />
+
+      <StyledLeaf1a src="/leaf1.svg" alt="leaf 1" />
+
+      <StyledLeaf2a src="/leaf2.svg" alt="leaf 2" />
+
+      <StyledLeaf1b src="/leaf1.svg" alt="leaf 1" />
+
+      <StyledLeaf2b src="/leaf2.svg" alt="leaf 2" />
 
     </StyledContainer>
   )
